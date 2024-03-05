07d26264d64def2e6273e6040afcb0320c6a30b9
Andrey Emelianov <621077@mail.ru>
2024-03-05 16:40:43 +0300

    реализовал обновление данных пользователя и получение текущего пользователя по jwt токену

--- controllesr/user-controller.js
+++ controllesr/user-controller.js
@@ -41,6 +41,7 @@ const UserController = {
       res.status(500).json({ error: 'Внутренняя ошибка сервера' });
     }
   },
+
   login: async (req, res) => {
     const { email, password } = req.body;
 
@@ -66,6 +67,7 @@ const UserController = {
       res.status(500).json({ error: 'Ошибка сервера' });
     }
   },
+
   getUserById: async (req, res) => {
     const { id } = req.params;
     const userId = req.user.userId;
@@ -87,15 +89,81 @@ const UserController = {
       });
 
       res.json({ ...user, isFollowing: Boolean(isFollowing) });
-    } catch (err) {}
-    console.log('Get current error', err);
-    res.status(500).json({ error: 'Ошибка сервера' });
+    } catch (err) {
+      console.log('Get current error', err);
+      res.status(500).json({ error: 'Ошибка сервера' });
+    }
   },
+
   updateUser: async (req, res) => {
-    res.send('updateUser');
+    const { id } = req.params;
+    const { email, name, dateOfBirth, bio, location } = req.body;
+
+    let filePath;
+
+    if (req.file && req.file.path) {
+      filePath = req.file.path;
+    }
+
+    if (id !== req.user.userId) {
+      return res.status(403).json({ error: 'Нет доступа' });
+    }
+
+    try {
+      if (email) {
+        const existingUser = await prisma.user.findFirst({ where: { email } });
+
+        if (existingUser && existingUser.id !== id) {
+          return res.status(400).json({ error: 'Такая почта уже зарегестрированна' });
+        }
+      }
+
+      const user = await prisma.user.update({
+        where: { id },
+        data: {
+          email: email || undefined,
+          name: name || undefined,
+          avatarUrl: filePath ? `/${filePath}` : undefined,
+          dateOfBirth: dateOfBirth || undefined,
+          bio: bio || undefined,
+          location: location || undefined,
+        },
+      });
+
+      res.json(user);
+    } catch (err) {
+      console.log('Ошибка обновления данных пользователя', err);
+      res.status(500).json({ error: 'Ошибка сервера' });
+    }
   },
+
   current: async (req, res) => {
-    res.send('current');
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id: req.user.userId },
+        include: {
+          followers: {
+            include: {
+              follower: true,
+            },
+          },
+          following: {
+            include: {
+              following: true,
+            },
+          },
+        },
+      });
+
+      if (!user) {
+        return res.status(400).json({ error: 'Не удалось найти пользователя' });
+      }
+
+      return res.status(200).json(user);
+    } catch (err) {
+      console.log('Get current error', err);
+      res.status(500).json({ error: 'Ошибка сервера' });
+    }
   },
 };
 
