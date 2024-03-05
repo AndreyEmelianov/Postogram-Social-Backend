03a575eb8ce794a101f7393e408f0298818187b5
Andrey Emelianov <621077@mail.ru>
2024-03-05 15:36:03 +0300

    реализовал логику логина и отправку jwt в ответ на успешный логин

--- controllesr/user-controller.js
+++ controllesr/user-controller.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const Jdenticon = require('jdenticon');
 const path = require('path');
 const fs = require('fs');
+const jwt = require('jsonwebtoken');
 
 const UserController = {
   register: async (req, res) => {
@@ -41,7 +42,29 @@ const UserController = {
     }
   },
   login: async (req, res) => {
-    res.send('login');
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ error: 'Все поля обязательны' });
+    }
+
+    try {
+      const user = await prisma.user.findUnique({ where: { email } });
+      if (!user) {
+        return res.status(400).json({ error: 'Неверный логин или пароль' });
+      }
+
+      const validPass = await bcrypt.compare(password, user.password);
+      if (!validPass) {
+        return res.status(400).json({ error: 'Неверный логин или пароль' });
+      }
+
+      const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY);
+      res.json({ token });
+    } catch (err) {
+      console.log('Login error', err);
+      res.status(500).json({ error: 'Ошибка сервера' });
+    }
   },
   getUserById: async (req, res) => {
     res.send('getUserById');
